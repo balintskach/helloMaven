e726c494a1432ca91d53377384cf5d069d81c0a0
Skach Bálint <balintskach@gmail.com>
2017-03-01 20:02:14 +0100

    First Commit.

---
+++ src/main/java/mik/HelloWorld.java
@@ -0,0 +1,55 @@
+package mik;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class helloWorld
+ */
+@WebServlet("/world")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    private String message;
+    private int count;
+    
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void init() throws ServletException{
+    	message = "Hello World!";
+    	count = 0;
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		Date date = new Date();
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "   " 
+		+ count + "</h1> <br> <h2>" + date + "</h2>");
+		count++;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
